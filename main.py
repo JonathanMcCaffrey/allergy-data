73835c24934e2bac49cc88cdf1c212c9c94dc6a4
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-08-21 20:50:48 -0400

    Collecting pollen data

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from datetime import datetime
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("https://www.theweathernetwork.com/ca/forecasts/pollen/ontario/ottawa")
+elem = driver.find_element_by_css_selector(".pollen .allergy-level")
+print(elem.text)
+
+
+
+with open('data.csv','a') as fd:
+    fd.write("\n\""+ datetime.now().strftime("%m/%d/%Y") + "\",\"" +elem.text + "\"")
+
 driver.close()
