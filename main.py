ca220dae6e8b53ae2405ca86e2a750921b37d8b7
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-08-21 22:27:07 -0400

    Removing email code

--- main.py
+++ main.py
@@ -15,29 +15,6 @@ driver.close()
 with open('data.csv','a') as fd:
     fd.write(pollenReport)
 
-# Get environment variables
-EMAIL = os.getenv('EMAIL')
-PASSWORD = os.environ.get('PASSWORD')
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
-# Create a secure SSL context
-context = ssl.create_default_context()
-
-# Try to log in to server and send email
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo() # Can be omitted
-    server.starttls(context=context) # Secure the connection
-    server.ehlo() # Can be omitted
-    server.login(EMAIL, PASSWORD)
-    # TODO: Send email here
-    server.sendmail(EMAIL, EMAIL, message)
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit() 
-
 # calculate stuff
 sys.stdout.write("::set-output " + pollenReport)
 sys.stdout.flush()
